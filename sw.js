cfe98e9619d23a20759844e2ca3be4a2cc8fb586
Robin Kranich <kontakt@robinkrani.ch>
2023-03-07 21:16:02 +0100

    added "offline-first" caching strategy for static assets

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
 self.addEventListener("fetch", () => {});
+
+// from https://web.dev/learn/pwa/caching/
+
+const urlsToCache = [
+    "/",
+    "bahnhofstafelsuche.webmanifest",
+    "favicon.ico",
+    "icon-192.png",
+    "stops.csv",
+    "turbocommons-es5.js",
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open("pwa-assets")
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => {
+                return cachedResponse || fetch(event.request);
+            })
+    );
+});
